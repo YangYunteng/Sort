68acd1d820cf95fb5f80fb6990570daa6c4a6359
YangYunteng <18302010022@fudan.edu.cn>
2019-11-30 23:22:09 +0800

    Main

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        int[] numbers={3,78,7,2,10,22};
+        InsertSort insertSort=new InsertSort(numbers);
+        insertSort.showResult();
+    }
+
+}
