5f5c9b17f8929ee76fbb693644d58525e153db35
YangYunteng <18302010022@fudan.edu.cn>
2019-12-03 22:20:14 +0800

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args){
-        int[] numbers={3,78,7,2,10,22};
-        InsertSort insertSort=new InsertSort(numbers);
-        insertSort.showResult();
+        int[] numbers={3,78,7,2,10,22,15};
+//        InsertSort insertSort=new InsertSort(numbers);
+//        insertSort.showResult();
+        ShellSort shellSort=new ShellSort(numbers);
+        shellSort.showResult();
+
     }
 
 }
